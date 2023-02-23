76c6e802ea10e6ed804f376c4a6afa1210d274d4
HC DIBS <75248199+HC-dibs@users.noreply.github.com>
2023-02-23 11:32:25 +0000

    Created an HTML Homepage

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) #object of a class always give it a name
-
+ 
 @app.route("/") #selects a path on a url
 def hello_world():
-  return "lets get it hans"
-# @ is a decorator in python
+  return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
